4c50adc0c7ca75be48623b9fe78b2484ad4693c0
chenqihui <zjhzcqh@163.com>
2023-03-15 20:56:42 +0800

    import and init

---
+++ 5-init/lib2/lib2.go
@@ -0,0 +1,11 @@
+package lib1
+
+import "fmt"
+
+func Lib2Test() {
+	fmt.Println("lib2Test...")
+}
+
+func init() {
+	fmt.Println("lib2Test init ...")
+}
---
+++ 5-init/main.go
@@ -0,0 +1,9 @@
+package main
+
+// "GolangDemo/5-init/lib1"
+// "GolangDemo/5-init/lib2"
+
+func main() {
+	lib1.lib1Test()
+	lib2.lib1Test()
+}
